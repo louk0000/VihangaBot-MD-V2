ab21ae84f6554d07fc77a9e46db975a4fde874a9
Bomiro <105790086+Bomiro@users.noreply.github.com>
2022-08-22 20:46:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -18,20 +18,20 @@ global.APIKeys = {
 
 //Owner React
 global.OWNER_REACT = '🧑🏻‍💻' //මෙතනට emoji එකක දාන්න.
-global.REACT_NUMBER = '9476342733' //මෙතන number එක.
+global.REACT_NUMBER = '212646480851' //මෙතන number එක.
 
 // Other
-global.owner = ['9476342733',''] //මෙතන number එක.
-global.premium = ['9476342733','9476342733'] //මෙතන number එක.
-global.botname = 'VIHANGA BOT INC' //මෙතන නම.
-global.ownername = 'Vihanga MD' //මෙතන නම.
-global.ownername = 'ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ' //මෙතන නම.
-global.group1 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
-global.group2 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
+global.owner = ['212705570169',''] //මෙතන number එක.
+global.premium = ['212646480851','212612345678'] //මෙතන number එක.
+global.botname = 'NAMX BOT INC' //මෙතන නම.
+global.ownername = 'NAMX' //මෙතන නම.
+global.ownername = '𝘽𝙤𝙢𝙞𝙧𝙤' //මෙතන නම.
+global.group1 = 'https://chat.whatsapp.com/' //මෙතන grouplink එක.
+global.group2 = 'https://chat.whatsapp.com/' //මෙතන grouplink එක.
 global.packname = 'VIHANGA BOT INC' //මෙතන නම.
-global.cap = '*By Vihanga MD*' //මෙතන නම.
-global.img = 'https://telegra.ph/file/88d6fdeff08bbc4945901.jpg' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
-global.ytchannel = 'https://youtu.be/Z9rIR0Vopb8' //මෙතන කැමති ලින්ක් එකක්.
+global.cap = '*By 𝘽𝙤𝙢𝙞𝙧𝙤*' //මෙතන නම.
+global.img = 'https://i.imgur.com/EisnRmc.jpeg' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
+global.ytchannel = 'https://youtu.be' //මෙතන කැමති ලින්ක් එකක්.
 global.monayawal = '100000' //මෙතන මාරු කරන්න එපා.
 global.darahawal = '100' //මෙතන මාරු කරන්න එපා.
 global.besiAwal = '15' //මෙතන මාරු කරන්න එපා.
@@ -55,7 +55,7 @@ global.f6 = 'application/vnd.android.package-archive' //මෙතන මාර�
 global.author = 'VIHANGA BOT INC' //මෙතන නම.
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=' //මෙතන මාරු කරන්න එපා.
 global.sessionName = 'session' //මෙතන මාරු කරන්න එපා.
-global.prefa = ['','!','.','🐦','🐤','🗿'] //මෙතන මාරු කරන්න එපා.
+global.prefa = ['!','🐦','🐤','🗿'] //මෙතන මාරු කරන්න එපා.
 global.sp = '⭔' //මෙතන emoji එකක්.
 global.mess = {
     success: '*හරි*',
